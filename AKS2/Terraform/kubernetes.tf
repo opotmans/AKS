933e27dea7a3bfd57b70ee2fb4c3fd2e6898f3d6
Olivier Potmans <potmans@hotmail.com>
2020-11-30 08:10:45 +0100

    introduction login-password acr

--- AKS2/Terraform/kubernetes.tf
+++ AKS2/Terraform/kubernetes.tf
@@ -1,5 +1,4 @@
 provider "kubernetes" {
-
 load_config_file = "false"
 host = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
 username = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
@@ -7,15 +6,33 @@ password = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
 client_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
 client_key = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
 cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-
 }
 
+//Definiion of a namespace "test"
 resource "kubernetes_namespace" "example" {
   metadata {
     name = "test"
   }
-
   depends_on = [
     azurerm_kubernetes_cluster.aks_cluster,
+  ]
+}
+
+
+//Definition of a secret to connect to the ACR
+ressource "kubernetes_secret" "acr" {
+  metadata {
+    name = "basic-auth"
+  }
+
+  data = {
+    username = "${azurerm_container_registry.acr.admin_username}"
+    password = "${azurerm_container_registry.acr.admin_password}"
+  }
+
+  type = "kubernetes.io/basic-auth"
+
+  depend_on = [
+      azurerm_container_registry.acr,
   ]
 }
