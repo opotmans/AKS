d82c4311f5310dc3cae5cadafd9f42f650315580
Olivier Potmans <potmans@hotmail.com>
2020-12-08 20:30:59 +0100

    helm corrected

--- AKS2/Terraform/helm.tf
+++ AKS2/Terraform/helm.tf
@@ -7,13 +7,14 @@ provider "helm" {
     client_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
     client_key = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+    load_config_file = false
   }
 }
 
 resource "helm_release" "mydatabase" {
   name  = "mydatabase"
   repository = "https://charts.helm.sh/stable"
-  chart = "stable/mysql"
+  chart = "mysql"
 
  // set {
  //   name  = "mariadbUser"
